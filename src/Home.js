21e7f43f21cec19a0406c74d762098947cb1481a
charmanderx <80680794+charmanderx@users.noreply.github.com>
2021-03-15 15:39:24 +0530

    kafene

---
+++ src/Home.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./App.css";
+import logo from "./logo.png";
+
+const Home = () => {
+  return (
+    <>
+      <div className="nav">
+        <div className="nav-left">
+          <img src={logo} alt="logo" id="logo" />
+          <p id="heading">Kafene</p>
+        </div>
+        <div className="nav-right">
+          <a href="" id="nav-links">
+            Orders
+          </a>
+          <a href="" id="nav-links">
+            Products
+          </a>
+          <a href="" id="nav-links">
+            Users
+          </a>
+        </div>
+      </div>
+      <div className="login-form">
+        <h2>Sign In</h2>
+        <input
+          className="login-input"
+          type="text"
+          name="username"
+          placeholder="Enter Username"
+        />
+        <input
+          className="login-input"
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+        />
+        <input type="submit" value="Login" className="login-btn" />
+      </div>
+    </>
+  );
+};
+
+export default Home;
